aa019e94b77196e9ab20d29cfb71f3d3adc0dae4
pmg617 <55558633+pmg617@users.noreply.github.com>
2019-11-12 09:23:09 +0100

    Add files via upload

--- intercom_binaural.py
+++ intercom_binaural.py
@@ -19,7 +19,7 @@ class Intercom_binaural(Intercom_bitplanes):
 
     def record_send_and_play_stereo(self, indata, outdata, frames, time, status):#es el record send and play del intercom buffer
         indata[:, 1] = np.subtract(indata[:, 1], indata[:, 0])      # al canal derecho se le resta el canal izquierdo, por lo que el resultado es que queda casi todo a 0(del derecho)
-        self.record_and_send(indata)
+        self.send(indata)
         self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
         chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
         chunk[:, 1] = np.add(chunk[:, 1], chunk[:, 0])              #al canal derecho se le suma el canal izquierdo, por lo que el resultado es que queda casi todo a 0(del derecho)
